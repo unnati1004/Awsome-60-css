52612334019528b5b5726de38ea4627dadcadfed
unnati1004 <unnatigandhi999@gmail.com>
2025-04-28 03:21:37 +0530

    project7

---
+++ project.js
@@ -0,0 +1,39 @@
+// data.js
+export let data = [
+     {
+       label: "Project 1",
+       link: "./Project1/index.html",
+       description: "Project1 checkbox"
+     },
+     {
+       label: "Project 2",
+       link: "./Project2/index.html",
+       description: "Project2 checkbox"
+     },
+     {
+       label: "Project 3",
+       link: "./Project3/index.html",
+       description: "Project3 checkbox"
+     },
+     {
+       label: "Project 4",
+       link: "./Project4/index.html",
+       description: "Project4 checkbox"
+     },
+     {
+       label: "Project 5",
+       link: "./Project5/index.html",
+       description: "Project5 checkbox"
+     },
+     {
+       label: "Project 6",
+       link: "./Project6/index.html",
+       description: "Project6 checkbox"
+     },
+     {
+      label: "Project 7",
+      link: "./Project7/index.html",
+      description: "Project7 checkbox"
+    }
+   ];
+   
