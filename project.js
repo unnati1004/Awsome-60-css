f69a38427733ae68f0c99a4eecb42861faa3b98e
unnati1004 <unnatigandhi999@gmail.com>
2025-04-30 14:54:27 +0530

    changes folder name to  public folder

--- project.js
+++ project.js
@@ -4,61 +4,61 @@ export let data = [
        label: "Project 1",
        link: "./Project1/index.html",
        description: "Project1 checkbox",
-       image: "../assets/project1.png" 
+       image: "../public/project1.png" 
       },
      {
        label: "Project 2",
        link: "./Project2/index.html",
        description: "Project2 checkbox",
-       image: "../assets/project2.png" 
+       image: "../public/project2.png" 
      },
      {
        label: "Project 3",
        link: "./Project3/index.html",
        description: "Project3 checkbox",
-        image: "../assets/project3.png"
+        image: "../public/project3.png"
      },
      {
        label: "Project 4",
        link: "./Project4/index.html",
        description: "Project4 checkbox",
-        image: "../assets/project4.png"
+        image: "../public/project4.png"
      },
      {
        label: "Project 5",
        link: "./Project5/index.html",
        description: "Project5 checkbox",
-        image: "../assets/project5.png"
+        image: "../public/project5.png"
      },
      {
        label: "Project 6",
        link: "./Project6/index.html",
        description: "Project6 checkbox",
-        image: "../assets/project6.png"
+        image: "../public/project6.png"
      },
      {
       label: "Project 7",
       link: "./Project7/index.html",
       description: "Project7 checkbox",
-       image: "../assets/project7.png"
+       image: "../public/project7.png"
     },
     {
       label: "Project 8",
       link: "./Project8/index.html",
       description: "Project8 checkbox",
-       image: "../assets/project8.png"
+       image: "../public/project8.png"
     },
     {
       label: "Project 9",
       link: "./Project9/index.html",
       description: "Project9 checkbox",
-       image: "../assets/project9.png"
+       image: "../public/project9.png"
     },
     {
       label: "Project 10",
       link: "./Project10/index.html",
       description: "Project10 checkbox",
-       image: "../assets/project10.png"
+       image: "../public/project10.png"
     }
    ];
    
