828fd5afbbeff21e1eeb3b8b3d4201a93f4ec7b9
unnati1004 <unnatigandhi999@gmail.com>
2025-04-30 15:59:12 +0530

    trying to fix images of projects

--- project.js
+++ project.js
@@ -4,61 +4,61 @@ export let data = [
        label: "Project 1",
        link: "./Project1/index.html",
        description: "Project1 checkbox",
-       image: "public/project1.png" 
+       image: "images/project1.png" 
       },
      {
        label: "Project 2",
        link: "./Project2/index.html",
        description: "Project2 checkbox",
-       image: "public/project2.png" 
+       image: "images/project2.png" 
      },
      {
        label: "Project 3",
        link: "./Project3/index.html",
        description: "Project3 checkbox",
-        image: "public/project3.png"
+        image: "images/project3.png"
      },
      {
        label: "Project 4",
        link: "./Project4/index.html",
        description: "Project4 checkbox",
-        image: "public/project4.png"
+        image: "images/project4.png"
      },
      {
        label: "Project 5",
        link: "./Project5/index.html",
        description: "Project5 checkbox",
-        image: "public/project5.png"
+        image: "images/project5.png"
      },
      {
        label: "Project 6",
        link: "./Project6/index.html",
        description: "Project6 checkbox",
-        image: "public/project6.png"
+        image: "images/project6.png"
      },
      {
       label: "Project 7",
       link: "./Project7/index.html",
       description: "Project7 checkbox",
-       image: "public/project7.png"
+       image: "images/project7.png"
     },
     {
       label: "Project 8",
       link: "./Project8/index.html",
       description: "Project8 checkbox",
-       image: "public/project8.png"
+       image: "images/project8.png"
     },
     {
       label: "Project 9",
       link: "./Project9/index.html",
       description: "Project9 checkbox",
-       image: "public/project9.png"
+       image: "images/project9.png"
     },
     {
       label: "Project 10",
       link: "./Project10/index.html",
       description: "Project10 checkbox",
-       image: "public/project10.png"
+       image: "images/project10.png"
     }
    ];
    
